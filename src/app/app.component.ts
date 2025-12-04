568608d666e10cbf9a740c9c108173e14e64d8f6
teaserfox <teaserfox@gmail.com>
2025-12-05 01:14:32 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tee_shop_agular';
+}
